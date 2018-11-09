12ae7c4e666f164f70a477870c456404a2e7fa21
Davide Trevisan <trevisan.davide94@libero.it>
2018-11-08 23:11:19 -0500

    improved tests

--- spec/spec.js
+++ spec/spec.js
@@ -3,10 +3,10 @@ import { GIBS_LAYERS, GIBS_MASKS } from '../lib/support/support';
 
 describe("gibs masks should work", () => {
     Object.keys(GIBS_MASKS).forEach(key => {
-        it(key + "should work", (done) => {
-            console.log('trying to fetch ' + GIBS_MASKS[key].template.replace('{Time}', '2018-11-03').replace('{TileMatrixSet}', 'GoogleMapsCompatible_Level6').replace('{TileMatrix}', '6').replace('{TileRow}', '18').replace('{TileCol}', '34'));
+        it(key + " should work", (done) => {
+            console.log('trying to fetch ' + GIBS_MASKS[key].template.replace('{Time}', '2018-11-03').replace('{TileMatrixSet}', 'GoogleMapsCompatible_Level' + GIBS_MASKS[key].zoom).replace('{TileMatrix}', '6').replace('{TileRow}', '18').replace('{TileCol}', '34'));
             // date is not relevant
-            axios.get(GIBS_MASKS[key].template.replace('{Time}', '2018-11-03').replace('{TileMatrixSet}', 'GoogleMapsCompatible_Level6').replace('{TileMatrix}', '6').replace('{TileRow}', '18').replace('{TileCol}', '34')).then(result => {
+            axios.get(GIBS_MASKS[key].template.replace('{Time}', '2018-11-03').replace('{TileMatrixSet}', 'GoogleMapsCompatible_Level' + GIBS_MASKS[key].zoom).replace('{TileMatrix}', '6').replace('{TileRow}', '18').replace('{TileCol}', '34')).then(result => {
                 console.log(' success :)');
                 expect(result.status).toBe(200); done();
             })
@@ -20,10 +20,10 @@ describe("gibs masks should work", () => {
 
 describe("all sources of gibs layers must exist and have yesterday images", () => {
     Object.keys(GIBS_LAYERS).forEach(key => {
-        it(key + "should work", (done) => {
-            console.log('trying to fetch ' + GIBS_LAYERS[key].template.replace('{Time}', '2018-11-03').replace('{TileMatrixSet}', 'GoogleMapsCompatible_Level6').replace('{TileMatrix}', '6').replace('{TileRow}', '18').replace('{TileCol}', '34'));
+        it(key + " should work", (done) => {
+            console.log('trying to fetch ' + GIBS_LAYERS[key].template.replace('{Time}', '2018-11-03').replace('{TileMatrixSet}', 'GoogleMapsCompatible_Level' + GIBS_LAYERS[key].zoom).replace('{TileMatrix}', '6').replace('{TileRow}', '18').replace('{TileCol}', '34'));
             // date is not relevant
-            axios.get(GIBS_LAYERS[key].template.replace('{Time}', '2018-11-03').replace('{TileMatrixSet}', 'GoogleMapsCompatible_Level6').replace('{TileMatrix}', '6').replace('{TileRow}', '18').replace('{TileCol}', '34')).then(result => {
+            axios.get(GIBS_LAYERS[key].template.replace('{Time}', '2018-11-03').replace('{TileMatrixSet}', 'GoogleMapsCompatible_Level' + GIBS_LAYERS[key].zoom).replace('{TileMatrix}', '6').replace('{TileRow}', '18').replace('{TileCol}', '34')).then(result => {
                 console.log(' success :)');
                 expect(result.status).toBe(200); done();
             })
