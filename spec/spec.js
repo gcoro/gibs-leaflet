8b20dc998fdea3678940d104fca225ad980e0ba5
Davide Trevisan <trevisan.davide94@libero.it>
2018-11-09 23:19:32 -0500

    updated support map and tests removed not found voices in wmts capabilities and updated the ones that I was able to find in the wmts capabilities, now all test passes!

--- spec/spec.js
+++ spec/spec.js
@@ -21,9 +21,9 @@ describe("gibs masks should work", () => {
 describe("all sources of gibs layers must exist and have yesterday images", () => {
     Object.keys(GIBS_LAYERS).forEach(key => {
         it(key + " should work", (done) => {
-            console.log('trying to fetch ' + GIBS_LAYERS[key].template.replace('{Time}', '2018-11-03').replace('{TileMatrixSet}', 'GoogleMapsCompatible_Level' + GIBS_LAYERS[key].zoom).replace('{TileMatrix}', '6').replace('{TileRow}', '18').replace('{TileCol}', '34'));
+            console.log('trying to fetch ' + GIBS_LAYERS[key].template.replace('{Time}', '2018-11-03').replace('{TileMatrixSet}', 'GoogleMapsCompatible_Level' + GIBS_LAYERS[key].zoom).replace('{TileMatrix}', '4').replace('{TileRow}', '6').replace('{TileCol}', '10'));
             // date is not relevant
-            axios.get(GIBS_LAYERS[key].template.replace('{Time}', '2018-11-03').replace('{TileMatrixSet}', 'GoogleMapsCompatible_Level' + GIBS_LAYERS[key].zoom).replace('{TileMatrix}', '6').replace('{TileRow}', '18').replace('{TileCol}', '34')).then(result => {
+            axios.get(GIBS_LAYERS[key].template.replace('{Time}', '2018-11-03').replace('{TileMatrixSet}', 'GoogleMapsCompatible_Level' + GIBS_LAYERS[key].zoom).replace('{TileMatrix}', '4').replace('{TileRow}', '6').replace('{TileCol}', '10')).then(result => {
                 console.log(' success :)');
                 expect(result.status).toBe(200); done();
             })
